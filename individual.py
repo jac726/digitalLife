610db364d9e75d3ca411ce4580fef2b7fa04426c
Joshua Church <jac726@cornell.edu>
2021-07-05 20:55:16 -0400

    add fight to individuals

--- individual.py
+++ individual.py
@@ -3,12 +3,14 @@ import random
 
 class Individual:
 
-    def __init__(self, size, baseStomach, costToMove, probToGrow=random.randint(1, 10)/10):
+    def __init__(self, size, baseStomach, costToMove, probToGrow=random.randint(1, 10)/10, probToLove=random.randint(1, 10)/10, strength=random.randint(1, 10)):
         self.individual = size
         self.stomachContents = baseStomach
         self.costToMove = costToMove
         self.probToGrow = probToGrow
         self.probToMove = 1 - probToGrow
+        self.probToLove = probToLove
+        self.strength = strength
 
     def getCostToMove(self):
         return self.costToMove
@@ -22,6 +24,12 @@ class Individual:
     def getProbToGrow(self):
         return self.probToGrow
 
+    def getProbToLove(self):
+        return self.probToLove
+
+    def getStrength(self):
+        return self.strength
+
     def mate(self, secondIndividual):
         parent1stomach = self.getStomachContents()
         parent2stomach = secondIndividual.getStomachContents()
@@ -34,4 +42,21 @@ class Individual:
         parent1ProbToGrow = self.getProbToGrow()
         parent2ProbToGrow = secondIndividual.getProbToGrow()
         childProbToGrow = parent1ProbToGrow + parent2ProbToGrow / 2
-        return Individual(childStomach, childMove, childProbToGrow)
+
+        parent1ProbToLove = self.getProbToLove()
+        parent2ProbToLove = secondIndividual.getProbToLove()
+        childProbToLove = parent1ProbToLove + parent2ProbToLove / 2
+
+        parent1strength = self.getStrength()
+        parent2strength = secondIndividual.getStrength()
+        childStrength = parent1strength + parent2strength / 2
+        return Individual(childStomach, childMove, childProbToGrow, childProbToLove, childStrength)
+
+    def fight(self, secondIndividual):
+
+        if self.strength > secondIndividual.strength:
+            return self
+        elif self.strength == secondIndividual.strength:
+            return self if random.random() <= .5 else secondIndividual
+        else:
+            return secondIndividual
